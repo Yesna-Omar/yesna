aca0f6e6040f114913595bbb72f8190df67e47da
Yesna-Omar <109865751+Yesna-Omar@users.noreply.github.com>
2022-08-06 13:52:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,170 +1,94 @@
-html{
-     
-    
-}
-body {
-    font-family: sans-serif;
-    font-weight: 500;
-    font-size: 1.8rem;
-    margin: 1.8rem;
-    background-color:black;
-    background-attachment:fixed;
-    margin: 0mm;
-    height: 0%;
-  
-}
-.c-site-header{
-    display: flex;
-    text-align: center;
-    color:#157bbf;
-    background-color:#FFFFC2;
-    padding:10px 10px 10px 10px;
-    justify-content: center;
-    width: 100%;
-    box-shadow: rgba(10, 10, 10, 10.4) 0px 2px 0px;
-}
-nav{
-     margin-right:90px;
-     display: flex;
- }
-.top{
-    text-align: center;
-    padding:58px 10px 10px 10px;
-    font-size:40px;
-    font-weight: bold;
-}
-#navigation{
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    color:#4863A0;
-    position: relative;
-    top: 5px; left: 40px;
-    list-style: none;
-}
-a:link { 
-    text-decoration: none;
-    color:#fc0a0a;
-    font-weight: bold;
-}
+/* =================
+  TESTS, LOOK AT THESE
+  Reading tests will always help you discover your requirements.
+  You can make this window bigger. 
+   ===================
+*/
+const {
+  core: { test, expect, run },
+  prettify
+} = window.jestLite;
 
-a:visited { text-decoration: none; }
+/* =================
+  FIND ELEMENTS
+  These are all the elements we will look for.
+   ===================
+*/
+const getHeader = document.querySelectorAll("header"),
+  getH1 = document.querySelectorAll("h1"),
+  getSiteHeader = document.querySelectorAll(".c-site-header"),
+  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
+  getMain = document.querySelectorAll("main"),
+  getFooter = document.querySelectorAll("footer"),
+  getSiteFooter = document.querySelectorAll(".c-site-footer"),
+  getIFrame = document.querySelectorAll("iframe"),
+  getImage = document.querySelectorAll("img"),
+  getWords = document.body.innerText;
+
+/* =================
+   ASSERTIONS 
+   These are the things we check are true about your page.
+   Read and update your HTML to discover the requirements.
+   The tests will run every time you update your code.
+   ===================
+*/
+
+var links, name2, item;
 
 
-a:hover { text-decoration: none; }
+links = ['https://www.facebook.com/codeyourfuture.io', 'https://twitter.com/codeyourfuture?lang=en', 'https://www.instagram.com/codeyourfuture_/?hl=en'];
+name2 = ['facebook', 'twitter', 'instagram'];
 
 
-a:active { text-decoration: none; }
+document.getElementById('button-cyf').addEventListener('click', (event) => {
+  let element_list_cyf = document.getElementById('list-cyf');
+  name2.forEach((item) => {
+    let new_li = document.createElement('li');
+    let new_a = document.createElement('a');
+    new_a.setAttribute("href", links.shift());
+    new_a.innerText = item;
+
+    new_li.appendChild(new_a);
+
+    element_list_cyf.appendChild(new_li);
+  });
+
+});
 
 
-.img-property{
-     border-radius: 10%;
-     height: auto;
-     padding:1%;
-     margin:auto;
-     position:absolute;
-     top:45%;
-     left:58%;
-     max-width: 75%;
-     max-height: 75%;
-}
-main{
-     max-width: 80%;
-     margin:auto;  
-}
-.about-me{
-     padding:40px;
-     margin:50px;
-     text-align:left;
-     font-size:32px;
-     color:white;
-     position:absolute;
-     left:10%;
-     top:220px;
-}
-.about-the-course{
-     width:500px;
-     padding:90px;
-     text-align:left;
-     font-size:28px;
-     color:white;
-}
-#about-the-course{
-     max-width: 80%;
-     position:absolute;
-     left:10%;
-     top:650px;
-}
-.video-title{
-     color:white;
-     padding:0 0 0 10%;
-     position:absolute;
-     top:148%;
-     left:28%;
-     font-size:40px;
-}
-.video{
-     border-radius: 10%;
-     width: 27vw;
-     height:25vw;
-     position:absolute;
-     top:160%;
-     left:37%;
-}
-#p-cyf{
-
-     font-size:28px;
-     color:white; 
-     text-align: center; 
-     font-size:40px; 
-     margin: 0%;
-     position: relative;
-     bottom: -1300pt;
 
 
-}
-#button-cyf{
-     font-size:40px; 
-     margin: 0%;
-     position: relative;
-     left:37%;
-     bottom: -1350pt;
-     color: #fc0a0a;
-     background-color:#FFFFC2;
-}
-#list-cyf{
-     text-align: center; 
-     font-size:40px; 
-     margin: 0%;
-     position: relative;
-     bottom: -1300pt;
-}
-.c-site-footer{
-     text-align: center;
-     color:#4863A0;
-     background-color:#FFFFC2;
-     padding:50px;
-     font-size:20px; 
-     margin: 0%;
-     position: relative;
-     bottom: -1500pt;
-     
-}
-.b{
-     padding:0 0 0 0%;
-}
-.c{
-     padding:0 0 0 0%;
-}
 
-#tests{
-     position: relative;
-     bottom: -1500pt;
-     
-}
+test("There is at least one header element", () => {
+  expect(getHeader.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one h1", () => {
+  expect(getH1.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one header element with the class c-site-header", () => {
+  expect(getSiteHeader.length).toBe(1);
+});
+test("There is a nav element with an aria-label of Main Site Links.", () => {
+  expect(getAria.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one main element", () => {
+  expect(getMain.length).toBe(1);
+});
+test("There is at least one footer element", () => {
+  expect(getFooter.length).toBeGreaterThanOrEqual(1);
+});
+test("There is only one footer element with the class c-site-footer", () => {
+  expect(getSiteFooter.length).toBe(1);
+});
+test("There is embedded video", () => {
+  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
+});
+test("There is at least one image", () => {
+  expect(getImage.length).toBeGreaterThanOrEqual(1);
+});
+test("There are at least 500 words on the page", () => {
+  expect(getWords.length).toBeGreaterThanOrEqual(500);
+});
 
-/* don't edit this */
-.jest-lite-report__status--pass {
-  background: var(--color-pass);
-}
-.jest-lite-report__errors { display:none }
+const console = document.getElementById("tests");
+prettify.toHTML(run(), console);vvvvvvvvvv
